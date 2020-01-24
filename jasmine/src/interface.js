74f92ecc6072385960630ef4bf10c6765669fbd0
Umberto Valente <uvalente89@gmail.com>
2020-01-24 12:05:49 +0000

    Added CSS styling

--- jasmine/src/interface.js
+++ jasmine/src/interface.js
@@ -15,9 +15,8 @@ $( document ).ready(function() {
     updateTemperature();
   });
 
-  $('.power-saving').click(function() {
+  $('input[type="checkbox"]').click(function() {
     thermostat.switchPowerSave();
-    updatePowerSavingMode();
     updateTemperature();
   });
 
@@ -25,16 +24,26 @@ $( document ).ready(function() {
 
   $('.power-usage').text(thermostat.getEnergyUsage());
 
+  $.get( 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=65f656b60f2f754b93b689557641dc3e&units=metric', function(json) {
+    let cityTemperature = Math.round(json.main.temp);
+    let cityHumidity = json.main.humidity;
+    $('.city-temperature').html(`<div class='api-temp'><img src='public/celsius.svg'> ${cityTemperature}</div><div class='api-hum'><img src='public/percent.svg'> ${cityHumidity}</div>`);
+  });
+
+  $('select').change(function() {
+    updateCityTemperature(($('select').val()));
+  })
+
   function updateTemperature() {
     $('#current-temperature').text(thermostat.getCurrentTemperature());
     $('#current-temperature').attr('class', thermostat.getEnergyUsage());
   };
 
-  function updatePowerSavingMode() {
-    if (thermostat.isPowerSaving()) {
-      $('.power-saving-status').text('on');
-    } else {
-      $('.power-saving-status').text('off');
-    }
+  function updateCityTemperature(city) {
+    $.get( `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=65f656b60f2f754b93b689557641dc3e&units=metric`, function(json) {
+      let cityTemperature = Math.round(json.main.temp);
+      let cityHumidity = json.main.humidity;
+      $('.city-temperature').html(`<div class='api-temp'><img src='public/celsius.svg'> ${cityTemperature}</div><div class='api-hum'><img src='public/percent.svg'> ${cityHumidity}</div>`);
+    });
   };
 });
